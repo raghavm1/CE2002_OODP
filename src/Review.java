70fed5ca01b9ac88b35727473976cb98545f8452
Laksh <lakshyajeet22@gmail.com>
2019-10-22 23:02:39 +0800

    Add addReview function

--- src/Review.java
+++ src/Review.java
@@ -1,17 +1,42 @@
-
+/**
+ * Represents a single movie review
+ * @author Lakshyajeet Dwivedee
+ *
+ */
 public class Review {
+	/**
+	 * The review of the movie
+	 */
 	private String review;
+	/**
+	 * The rating given to the movie
+	 * Must be between 1 and 5
+	 */
 	private int rating;
 	
+	/**
+	 * Creates a new Review with the given review and rating
+	 * Both review and rating must be given
+	 * @param review Review of the movie
+	 * @param rating Rating of the movie
+	 */
 	public Review(String review, int rating) {
 		this.review = review;
 		this.rating = rating;
 	}
 	
+	/**
+	 * Return the text of this review
+	 * @return The review text
+	 */
 	public String getReview() {
 		return this.review;
 	}
 	
+	/**
+	 * Return the rating given in this review
+	 * @return The review rating
+	 */
 	public int getRating() {
 		return rating;
 	}
--- src/Reviews.java
+++ src/Reviews.java
@@ -1,11 +1,38 @@
 import java.util.*;
 
+/**
+ * Represents all the reviews and the overall rating of a movie
+ * @author Lakshyajeet Dwivedee
+ *
+ */
 public class Reviews {
+	/**
+	 * A list of all reviews of the movie
+	 */
 	private ArrayList<Review> reviewList;
+	/*
+	 * The overall rating of the movie
+	 * Must be between 1 and 5
+	 * Must have precision of 1 decimal place
+	 */
 	private float averageRating;
 	
+	/**
+	 * Creates a new empty Reviews 
+	 */
 	public Reviews() {
 		reviewList = new ArrayList<Review>();
 		averageRating = 0;
+	}
+	
+	/**
+	 * Adds a new review to the review list and updates the average rating
+	 * Both review and rating must be specified
+	 * @param review Review of the movie given by the user
+	 * @param rating Rating of the movie given by the user
+	 */
+	public void addReview(String review, int rating) {
+		reviewList.add(new Review(review, rating));
+		averageRating = ((averageRating)*(reviewList.size()-1) + rating)/reviewList.size();
 	}
 }
