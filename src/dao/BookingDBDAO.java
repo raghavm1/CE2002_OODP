33cbfaa8db2cbb5f55cbb61b10367d1627386892
Laksh <lakshyajeet22@gmail.com>
2019-11-13 15:22:10 +0800

    Add improved CSV functionality

--- src/dao/BookingDBDAO.java
+++ src/dao/BookingDBDAO.java
@@ -4,6 +4,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 import entities.Booking;
+import utilities.CSVFileIO;
+import utilities.CSVRow;
 
 /**
  * @author Lakshyajeet Dwivedee
@@ -12,9 +14,39 @@ public class BookingDBDAO {
 
     public void insert(Booking booking, List<Booking> bookingList) {
     	bookingList.add(booking);
+    	this.saveToFile(booking);
+    }
+    
+    private void saveToFile(Booking booking) {
+    	CSVRow tempCsvRow = new CSVRow();
+    	tempCsvRow.clearRow();
+    	
+    	tempCsvRow.addVariable(booking.getTransactionID());
+    	tempCsvRow.addVariable(booking.getCustomerName());
+    	tempCsvRow.addVariable(booking.getMovieName());
+    	tempCsvRow.addVariable(booking.getEmailID());
+    	tempCsvRow.addVariable(String.valueOf(booking.getMobileNumber()));
+    	tempCsvRow.addVariable(booking.getCinemaHallID());
+    	tempCsvRow.addVariable(booking.getCineplexName());
+    	
+    	CSVFileIO.writeToCSV("../bookings.csv", tempCsvRow);
+    }
+    
+    private List<Booking> convertToBookings(List<CSVRow> table) {
+    	List<Booking> bookings = new ArrayList<>();
+    	for(CSVRow csvRow : table) {
+    		List<String> row = csvRow.getRow();
+    		Booking tempBooking = new Booking(row.get(1), row.get(3), Integer.parseInt(row.get(4)), row.get(2), row.get(5), row.get(6), row.get(0));
+    		bookings.add(tempBooking);
+    	}
+    	return bookings;
     }
 
-    public List<Booking> getBookings(List<Booking> bookingList, String emailID) {
+    public List<Booking> getBookings(String emailID) {
+    	
+    	List<CSVRow> table = CSVFileIO.getParsedCSV("../bookings.csv");
+    	List<Booking> bookingList = convertToBookings(table);
+    	
         List<Booking> relevantBookings = new ArrayList<Booking>();
         for(Booking booking : bookingList) {
         	if(booking.getEmailID().compareTo(emailID) == 0) {
--- src/entities/Booking.java
+++ src/entities/Booking.java
@@ -14,12 +14,12 @@ public class Booking {
 	private String customerName;
 	private String emailID;
 	private int mobileNumber;
-	private Movie movie;
-	private CinemaHall cinemaHall;
-	private Cineplex cineplex;
+	private String movieName;
+	private String cinemaHallID;
+	private String cineplexName;
 	
 	/**
-     * Constructor
+     * Constructor called when a new booking is made
      *
      * @param customerName Name of customer making the booking
      * @param emailID Email ID entered by customer making the booking
@@ -28,17 +28,78 @@ public class Booking {
      * @param cinemaHall Cinema hall in which movie was shown
      * @param cinePlex Cineplex in which movie was booked
      */
-	public Booking(String customerName, String emailID, int mobileNumber, Movie movie, CinemaHall cinemaHall, Cineplex Cineplex) {
+	public Booking(String customerName, String emailID, int mobileNumber, String movieName, String cinemaHallID, String cineplexName) {
 		this.customerName = customerName;
 		this.emailID = emailID;
 		this.mobileNumber = mobileNumber;
-		this.movie = movie;
-		this.cinemaHall = cinemaHall;
-		this.cineplex = cineplex;
-		this.transactionID = String.valueOf(LocalDateTime.now().getYear()) + String.valueOf(LocalDateTime.now().getMonthValue()) + String.valueOf(LocalDateTime.now().getDayOfYear()) + String.valueOf(LocalDateTime.now().getHour()) + String.valueOf(LocalDateTime.now().getMinute()) + String.valueOf(cinemaHall.getHallNumber());
+		this.movieName = movieName;
+		this.cinemaHallID = cinemaHallID;
+		this.cineplexName = cineplexName;
+		this.transactionID = String.valueOf(LocalDateTime.now().getYear()) + String.valueOf(LocalDateTime.now().getMonthValue()) + String.valueOf(LocalDateTime.now().getDayOfYear()) + String.valueOf(LocalDateTime.now().getHour()) + String.valueOf(LocalDateTime.now().getMinute()) + String.valueOf(cinemaHallID);
+	}
+	
+	/**
+     * Constructor called when an older booking is being retrieved
+     *
+     * @param customerName Name of customer making the booking
+     * @param emailID Email ID entered by customer making the booking
+     * @param mobileNumber Mobile number entered by the customer making the booking
+     * @param movie Movie for which booking is being made
+     * @param cinemaHall Cinema hall in which movie was shown
+     * @param cinePlex Cineplex in which movie was booked
+     * @param transactionID Transaction ID of this booking
+     */
+	public Booking(String customerName, String emailID, int mobileNumber, String movieName, String cinemaHallID, String cineplexName, String transactionID) {
+		this.customerName = customerName;
+		this.emailID = emailID;
+		this.mobileNumber = mobileNumber;
+		this.movieName = movieName;
+		this.cinemaHallID = cinemaHallID;
+		this.cineplexName = cineplexName;
+		this.transactionID = transactionID;
 	}
 	
 	public String getEmailID() {
 		return this.emailID;
+	}
+	
+	public String getCustomerName() {
+		return this.customerName;
+	}
+	
+	public int getMobileNumber() {
+		return this.mobileNumber;
+	}
+	
+	public String getMovieName() {
+		return this.movieName;
+	}
+	
+	public String getCinemaHallID() {
+		return this.cinemaHallID;
+	}
+	
+	public String getCineplexName() {
+		return this.cineplexName;
+	}
+	
+	public String getTransactionID() {
+		return this.transactionID;
+	}
+	
+	public void printBooking() {
+		System.out.print(this.transactionID);
+		System.out.print(" | ");
+		System.out.print(this.customerName);
+		System.out.print(" | ");
+		System.out.print(this.movieName);
+		System.out.print(" | ");
+		System.out.print(this.emailID);
+		System.out.print(" | ");
+		System.out.print(this.mobileNumber);
+		System.out.print(" | ");
+		System.out.print(this.cinemaHallID);
+		System.out.print(" | ");
+		System.out.println(this.cineplexName);
 	}
 }
